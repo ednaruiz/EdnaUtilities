ca558c6bc9fd4d53f2302308bc8330cbb23be1e3
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2017-09-21 17:14:15 +0200

    Create MakeSimpleAnalysis.py

---
+++ MakeSimpleAnalysis.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import argparse
+
+os.system('hawc-sw')
+
+parser = argparse.ArgumentParser("Process simple analysis")
+
+parser.add_argument('--source',help = "Source Config File", dest = 'source',required = True)
+parser.add_argument('--cuts',help="Cuts Config File",dest = 'cuts',required = True)
+parser.add_argument('--out',help = "Outdir",dest="out",required = True)
+parser.add_argument('--weight',help = "Weights file",dest="weight",required = True)
+
+args = parser.parse_args()
+
+os.system("mkidr %s "%(args.out))
+
+nBins = len(np.loadtxt(args.cuts))
+
+dump = "simple-analysis-dump --outdir %s --source-file %s --cuts-file %s"%(args.out,args.source,args.cuts)
+
+for iBins in range (0,nBins):
+  background = background + "simple-analysis-background -o %s -b %i -e 1.0 && "%(args.out,iBins)
+  smooth = smooth + "simple-analysis-smooth --outdir %s -b %i -r 0.2 --gauss && "%(args.out,iBins)
+  significance = significance + "simple-analysis-significance --outdir %s -b %i -w %s -p && "%(args.out,iBins,args.weight)
+os.system("%s %s %s %s"%(dump,background,smooth,significance))
+
+
