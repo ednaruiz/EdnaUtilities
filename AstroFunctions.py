d6026a8719d8884b571b344a48115ad53114a251
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2017-09-26 20:34:52 +0200

    Update AstroFunctions.py

--- AstroFunctions.py
+++ AstroFunctions.py
@@ -9,3 +9,16 @@ def RightAscension():
   degrees = hours*(15) + minutes*(1/4.) + seconds*(1/240.)
   print(degrees) 
   
+def DeclinationHAWC():  
+  days = float(raw_input("d: ")) 
+  minutes = float(raw_input("m: ")) 
+  seconds = float(raw_input("s: ")) 
+  degrees = hours*(1) + minutes*(1/60.) + seconds*(1/3600.)
+  print(degrees) 
+ 
+def DeclinationHESS():  
+  days = float(raw_input("d: ")) 
+  minutes = float(raw_input("m: ")) 
+  seconds = float(raw_input("s: ")) 
+  degrees = hours*(1) - minutes*(1/60.) - seconds*(1/3600.)
+  print(degrees) 
