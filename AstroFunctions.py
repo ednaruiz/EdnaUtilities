811b2bf1f3c9a2c6586b1617b9e975d9c66cf9b1
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2017-09-26 20:31:15 +0200

    Update AstroFunctions.py

--- AstroFunctions.py
+++ AstroFunctions.py
@@ -6,17 +6,6 @@ def RightAscension():
   hours = float(raw_input("h: ")) 
   minutes = float(raw_input("m: ")) 
   seconds = float(raw_input("s: ")) 
-  degrees = hours*(1/24.) + minutes*(1/(24.*60.) + seconds*()
+  degrees = hours*(15) + minutes*(1/4.) + seconds*(1/240.)
   print(degrees) 
   
-  
-  The highest point in the sky, 
-  called meridian, is the projection of a longitude 
-  line onto the celestial sphere. Since a complete circle
-  contains 24h of right ascension or 360° (degrees of arc),  
-  1⁄24 of a circle is measured as 1h of right ascension, or 15°;  
-  1⁄(24×60) of a circle is measured as 1m of right ascension, or 15 
-  minutes of arc (also written as 15′); and  1⁄(24×60×60) 
-  of a circle contains 1s of right ascension, or 15 seconds 
-  of arc (also written as 15″). A full circle, measured in 
-  right-ascension units, contains 24 × 60 × 60 = 86 400s, or 24 × 60 = 1 440m, or 24h.
