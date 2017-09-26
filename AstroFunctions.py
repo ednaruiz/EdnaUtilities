8bb7da9728fa518930e434310b72efe3c63a2474
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2017-09-26 20:42:37 +0200

    Update AstroFunctions.py

--- AstroFunctions.py
+++ AstroFunctions.py
@@ -3,6 +3,7 @@ import math
 
 
 def RightAscension():
+  degrees = float(raw_input("degrees: ")) 
   hours = float(raw_input("h: ")) 
   minutes = float(raw_input("m: ")) 
   seconds = float(raw_input("s: ")) 
@@ -22,3 +23,55 @@ def DeclinationHESS():
   seconds = float(raw_input("s: ")) 
   degrees = hours*(1) - minutes*(1/60.) - seconds*(1/3600.)
   print(degrees) 
+  
+  
+def deg2HMS(ra='', dec='', round=False):
+  RA, DEC, rs, ds = '', '', '', ''
+  if dec:
+    if str(dec)[0] == '-':
+      ds, dec = '-', abs(dec)
+    deg = int(dec)
+    decM = abs(int((dec-deg)*60))
+    if round:
+      decS = int((abs((dec-deg)*60)-decM)*60)
+    else:
+      decS = (abs((dec-deg)*60)-decM)*60
+    DEC = '{0}{1} {2} {3}'.format(ds, deg, decM, decS)
+  
+  if ra:
+    if str(ra)[0] == '-':
+      rs, ra = '-', abs(ra)
+    raH = int(ra/15)
+    raM = int(((ra/15)-raH)*60)
+    if round:
+      raS = int(((((ra/15)-raH)*60)-raM)*60)
+    else:
+      raS = ((((ra/15)-raH)*60)-raM)*60
+    RA = '{0}{1} {2} {3}'.format(rs, raH, raM, raS)
+  
+  if ra and dec:
+    return (RA, DEC)
+  else:
+    return RA or DEC
+  
+  
+def HMS2deg(ra='', dec=''):
+  RA, DEC, rs, ds = '', '', 1, 1
+  if dec:
+    D, M, S = [float(i) for i in dec.split()]
+    if str(D)[0] == '-':
+      ds, D = -1, abs(D)
+    deg = D + (M/60) + (S/3600)
+    DEC = '{0}'.format(deg*ds)
+  
+  if ra:
+    H, M, S = [float(i) for i in ra.split()]
+    if str(H)[0] == '-':
+      rs, H = -1, abs(H)
+    deg = (H*15) + (M/4) + (S/240)
+    RA = '{0}'.format(deg*rs)
+  
+  if ra and dec:
+    return (RA, DEC)
+  else:
+    return RA or DEC
