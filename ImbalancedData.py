944fb279db0b9f097a78ee17da9ef3fc73cf7689
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2017-11-22 22:09:09 +0100

    Update ImbalancedData.py

--- ImbalancedData.py
+++ ImbalancedData.py
@@ -15,11 +15,21 @@ class undersample:
       print("Data is not a pandas DataFrame!")
       break
     
+    # i am very sorry this is going to be so raw and trivial,
+    # i hope some day I can make it better
+    
+    maxv = max(data[column_name])
+    minv = min(data[column_name])
+    bining = np.linspace(maxv,minv,bins)
+    counts = []
+    for iBin in range (0,len(bining)-1):
+      counts.append(len(data[column_name][data[column_name]>bining[iBin] and data[column_name]<bininb[iBin+1]]))
+    limit = min(counts)
     
     return under_data
   
-  def plot_raw(self):
+  #def plot_raw(self):
     # plot the raw input
   
-  def plot_undersampled(self):
+  #def plot_undersampled(self):
     
