589676725283c3e489df1fb80892cb628e6693f9
Edna Ruiz-Velasco <edna.ruve@gmail.com>
2017-12-20 10:22:32 +0100

    Update xcdf-pass5.py

--- HAWC/xcdf-pass5.py
+++ HAWC/xcdf-pass5.py
@@ -1,1 +1,15 @@
+import numpy as np
+import os
+import sys
+import argparse 
+
+
+
+parser = argparse.ArgumentParser("Process a given list of xcdf file to apply pass 5 cuts")
+
+parser.add_argument('--files', help="list of files to process", dest = 'files')
+parser.add_argument('--cuts', help="cuts to apply",dest = 'cuts')
+
+args = parser.parse_args()
+
 
